d102e2f106c7237a5be3aa3d086cad8bc10fe53e
Isaac Medina <isaa.medina@duocuc.cl>
2024-06-26 04:52:38 -0400

    Clasificacion de saldos

--- MedinaIsaac.py
+++ MedinaIsaac.py
@@ -10,3 +10,14 @@ clientes = [f'cliente {i+1}' for i in range(10)]
 
 # Asignar saldos aleatorios
 saldos = {cliente: round(random.uniform(0,900), 3) for cliente in clientes}
+
+
+# Clasificacion de saldos
+clasificacion = {}
+for cliente, saldo in saldos.items():
+    if saldo < 300:
+        clasificacion[cliente] = ('bajo', saldo)
+    elif 300 <= saldo < 600:
+        clasificacion[cliente] = ('medio', saldo)
+    else:
+        clasificacion[cliente] = ('alto', saldo)    
