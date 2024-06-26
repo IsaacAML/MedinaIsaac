2893f7c794fe1f1af5352438f18dd0630963d8f0
Isaac Medina <isaa.medina@duocuc.cl>
2024-06-26 04:46:09 -0400

    Asignar saldos aleatorios

--- MedinaIsaac.py
+++ MedinaIsaac.py
@@ -6,3 +6,7 @@ import random, statistics, csv
 # Crear lista de clientes
  
 clientes = [f'cliente {i+1}' for i in range(10)]
+
+
+# Asignar saldos aleatorios
+saldos = {cliente: round(random.uniform(0,900), 3) for cliente in clientes}
